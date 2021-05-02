0e4ed4abd5c4cf3f54c54e38246152295b79b784
Ken Ober <kenneth.ober@gmail.com>
2021-05-02 08:21:42 -0400

    add a few functions

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// Partially apply a function
 const partial = (fn, ...argsNow) => (...argsLater) => fn(...argsNow, ...argsLater)
 
-export { partial }
+// When you want a Set but don't want to give up Array methods...
+const unique = arr => [...new Set(arr)]
+
+// Return a map with a one (key) to many (values) relationship
+// [[k1, v1], [k1, v1], [k1, v2], ...] => { k1: [v1, v1, v2, ...], ... }
+const arrayMap = keyVals => keyVals.reduce((acc, [k, v]) => {
+  acc[k] ??= []
+  acc[k].push(v)
+  return acc
+}, {})
+
+// Return an array-map with no duplicates in the array-values
+// [[k1, v1], [k1, v1], [k1, v2], ...] => { k1: [v1, v2, ...], ... }
+const arrayMapUnique = keyVals =>
+  Object.fromEntries(Object.entries(arrayMap(keyVals)).map(([k, v]) => [k, unique(v)]))
+
+export { partial, unique, arrayMap, arrayMapUnique }
