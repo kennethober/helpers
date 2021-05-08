cecbc3a28c8fc35b297497070e714c1be24da799
Ken Ober <kenneth.ober@gmail.com>
2021-05-08 18:24:29 -0400

    lint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// Function, ...any => Function  
+// Function, ...any => Function
 // Partially applies a function.
 const partial = (fn, ...argsNow) => (...argsLater) => fn(...argsNow, ...argsLater)
 
-// [[k1, v1], [k1, v1], [k1, v2], ...] => { k1: [v1, v1, v2, ...], ... }  
+// [[k1, v1], [k1, v1], [k1, v2], ...] => { k1: [v1, v1, v2, ...], ... }
 // Returns an Object mapping String|Symbol keys to Arrays of values.
 const collect = keyVals => keyVals.reduce((acc, [k, v]) => {
   acc[k] ??= []
@@ -10,7 +10,7 @@ const collect = keyVals => keyVals.reduce((acc, [k, v]) => {
   return acc
 }, {})
 
-// [[k1, v1], [k1, v1], [k1, v2], ...] => { k1: Set(v1, v2, ...), ... }  
+// [[k1, v1], [k1, v1], [k1, v2], ...] => { k1: Set(v1, v2, ...), ... }
 // Like collect(), but aggregates values into Sets.
 const collectUnique = keyVals => keyVals.reduce((acc, [k, v]) => {
   acc[k] ??= new Set()
