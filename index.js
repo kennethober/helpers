793ca09bce9ffc3f55251ba59ca2aefc498675c5
Ken Ober <kenneth.ober@gmail.com>
2021-05-09 09:10:52 -0400

    add doc generation

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-// Function, ...any => Function
-// Partially applies a function.
-const partial = (fn, ...argsNow) => (...argsLater) => fn(...argsNow, ...argsLater)
+/**
+ * Partially apply a function.
+ * @param {function} fn - Function to partially apply.
+ * @param {...*} args - Arguments to apply.
+ * @returns {function} Function with the provided arguments applied.
+ */
+const partial = (fn, ...args) => (...argsLater) => fn(...args, ...argsLater)
 
-// [[k1, v1], [k1, v1], [k1, v2], ...] => { k1: [v1, v1, v2, ...], ... }
-// Returns an Object mapping String|Symbol keys to Arrays of values.
+/**
+ * Collect values with the same key and return an object with a one-to-many key-value relationship.
+ * @param {array<array<(string|symbol), *>>} keyVals - Key-value pairs to reduce over.
+ * @returns {object<(string|symbol), array<*>>} Object mapping keys to arrays of values.
+ */
 const collect = keyVals => keyVals.reduce((acc, [k, v]) => {
   acc[k] ??= []
   acc[k].push(v)
   return acc
 }, {})
 
-// [[k1, v1], [k1, v1], [k1, v2], ...] => { k1: Set(v1, v2, ...), ... }
-// Like collect(), but aggregates values into Sets.
+/**
+ * Like collect(), but eliminates duplicate values (based on set behavior).
+ * @param {array<array<(string|symbol), *>>} keyVals - Key-value pairs to reduce over.
+ * @returns {object<(string|symbol), set<*>>} Object mapping keys to sets of unique values.
+ */
 const collectUnique = keyVals => keyVals.reduce((acc, [k, v]) => {
   acc[k] ??= new Set()
   acc[k].add(v)
   return acc
 }, {})
 
-// Number, Function, String (optional) => undefined
-// Call a sync|async function a number of times and log the total duration to the console.
+/**
+ * Call a function a number of times and log the total duration to the console.
+ * @async
+ * @param {number} times - The number of times to call a given function; useful for amplifying differences in execution time between functions.
+ * @param {function} fn - The function to call; may be synchronous or asynchronous.
+ * @param {string} [timerId] An id/label for the console timer; shows up in the log.
+ * @returns {undefined} Function called for side-effect only.
+ */
 const logTime = async (times, fn, timerId = `${Date.now()}-${Math.random()}`) => {
   console.time(timerId)
   for (let i = 1; i <= times; i++) { await fn() }
